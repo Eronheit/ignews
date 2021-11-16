41cd6e5eac9ecda44308bad9dfe8274129da0267
Eronheit <aquinoeronaldo@gmail.com>
2021-11-16 14:07:18 -0300

    feat: add authetication with Next Auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,23 +1,26 @@
+import { signIn, signOut, useSession } from 'next-auth/client';
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
-  return isUserLoggedIn ? (
+  const [session] = useSession();
+
+  return session ? (
     <button 
       type='button'
       className={styles.signInButton}
     >
       <FaGithub color="#04d361" />
-      Eronaldo Aquino
-      <FiX color="#737380" className={styles.closeIcon} />
+      {session.user.email}
+      <FiX color="#737380" className={styles.closeIcon} onClick={() => signOut()} />
     </button>
   ) : (
     <button 
       type='button'
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" />
       Sign in with Github
